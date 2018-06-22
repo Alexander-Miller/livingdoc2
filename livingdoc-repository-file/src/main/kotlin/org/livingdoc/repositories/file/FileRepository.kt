1d91f359ef721bb549f47b4ac8912f648a0c1d51
Alexander Miller <alexander.miller@novatec-gmbh.de>
2018-06-22 15:04:47 +0200

    Implement document-parsing of file repositories.

--- livingdoc-repository-file/src/main/kotlin/org/livingdoc/repositories/file/FileRepository.kt
+++ livingdoc-repository-file/src/main/kotlin/org/livingdoc/repositories/file/FileRepository.kt
@@ -3,15 +3,26 @@ package org.livingdoc.repositories.file
 import org.livingdoc.repositories.Document
 import org.livingdoc.repositories.DocumentFormat
 import org.livingdoc.repositories.DocumentRepository
+import org.livingdoc.repositories.format.HtmlFormat
+import org.livingdoc.repositories.format.MarkdownFormat
+import java.io.File
+import java.nio.file.Paths
 
 class FileRepository(
-    private val name: String,
-    private val format: DocumentFormat,
-    private val config: FileRepositoryConfig
+        private val name: String,
+        private val config: FileRepositoryConfig
 ) : DocumentRepository {
 
     override fun getDocument(documentIdentifier: String): Document? {
-        throw IllegalStateException("not implemented")
+        val file = Paths.get(config.documentRoot, documentIdentifier).toFile()
+        return determineFormatType(file)?.parse(file.inputStream())
     }
+
+    private fun determineFormatType(file: File): DocumentFormat? =
+            when (file.extension) {
+                ".md" -> MarkdownFormat()
+                ".html" -> HtmlFormat()
+                else -> null
+            }
 
 }
